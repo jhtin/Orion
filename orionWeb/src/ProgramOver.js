3988a7c1233aee0f984c66cfa02775b60fc1b5be
kyaw <khtin@student.unimelb.edu.au>
2018-03-25 08:52:01 +1100

    Program Participants can be seen in a table now

--- orionWeb/src/ProgramOver.js
+++ orionWeb/src/ProgramOver.js
@@ -1,11 +1,52 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react'
+import { Icon, Card, Container, Grid, Header, Divider } from 'semantic-ui-react';
+import ProgramOverFeed from './ProgramOverFeed';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 class ProgramOver extends React.Component {
+  state = {
+    programname: "Local Governance Program"
+  }
   render() {
     return (
       <Container>
-        Overview
+        <Grid padded>
+        <Grid.Row>
+          <Header as='h1'>{this.state.programname}</Header>
+        </Grid.Row>
+        <Divider/>
+
+        <Grid.Row>
+          <Grid.Column width={6}>
+        <Card>
+          <Map refs='map' center={[0,100]} zoom={6}>
+          <TileLayer
+            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+            url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          />
+          </Map>
+          <Card.Content extra>
+          <a>
+            <Icon name='user' />
+            200 Participants
+          </a>
+        </Card.Content>
+
+
+        </Card>  
+        </Grid.Column>
+        <Grid.Column width={10}>
+        More data here
+        </Grid.Column>
+  
+      </Grid.Row>
+        <Divider/>
+        <Grid.Row>
+          <Container>
+          <ProgramOverFeed/>
+          </Container>
+        </Grid.Row>
+        </Grid>
       </Container>
     )
   }
