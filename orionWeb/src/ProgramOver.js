93f72b49025c46c5e0d5de53ee1c09acaff1d3c7
Hazim J <hazim.jumali@gmail.com>
2018-03-25 11:32:57 +1100

    sfdbhjfdjndfjdnfjndf

--- orionWeb/src/ProgramOver.js
+++ orionWeb/src/ProgramOver.js
@@ -1,15 +1,38 @@
 import React from 'react';
-import { Icon, Card, Container, Grid, Header, Divider } from 'semantic-ui-react';
+import { Icon, Card, Container, Grid, Header, Divider, Image } from 'semantic-ui-react';
 import ProgramOverFeed from './ProgramOverFeed';
 import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
+import { VictoryPie, VictoryAnimation, VictoryLabel } from 'victory';
 
 class ProgramOver extends React.Component {
   state = {
-    programname: "Local Governance Program"
+    programname: "Human Development Initiatives",
+    percent: 73,
+    data: this.getData(73),
+  }
+
+  componentDidMount() {
+    // let percent = 25;
+    // this.setStateInterval = window.setInterval(() => {
+    //   percent += (Math.random() * 25);
+    //   percent = (percent > 100) ? 0 : percent;
+    //   this.setState({
+    //     percent, data: this.getData(percent)
+    //   });
+    // }, 2000);
+  }
+
+  componentWillUnmount() {
+    // window.clearInterval(this.setStateInterval);
+  }
+
+  getData(percent) {
+    return [{ x: 1, y: percent }, { x: 2, y: 100 - percent }];
   }
   render() {
     return (
       <Container>
+
         <Grid padded>
         <Grid.Row>
           <Header as='h1'>{this.state.programname}</Header>
@@ -18,33 +41,26 @@ class ProgramOver extends React.Component {
 
         <Grid.Row>
           <Grid.Column width={6}>
-        <Card>
-          <Map refs='map' center={[0,100]} zoom={6}>
+          <Header as='h3'>Participant distribution</Header>
+          <Divider/>
+          <Map refs='map' center={[0,100]} zoom={6} >
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           />
           </Map>
-          <Card.Content extra>
-          <a>
-            <Icon name='user' />
-            200 Participants
-          </a>
-        </Card.Content>
 
-
-        </Card>  
         </Grid.Column>
         <Grid.Column width={10}>
-        More data here
+          <Header as='h3'>Data Feed</Header>
+          <Divider/>
+          <ProgramOverFeed/>
         </Grid.Column>
-  
-      </Grid.Row>
+
+        </Grid.Row>
         <Divider/>
         <Grid.Row>
-          <Container>
-          <ProgramOverFeed/>
-          </Container>
+
         </Grid.Row>
         </Grid>
       </Container>
@@ -55,3 +71,54 @@ class ProgramOver extends React.Component {
 export default ProgramOver;
 
 //ReactDOM.render(<CustomTheme/>, mountNode)
+
+// <Grid columns={2} centered={false}>
+//   <Grid.Row>
+//     <Grid.Column>
+//       <Map refs='map' center={[0,100]} zoom={6} style={{height: 600, width: 600}}>
+//       <TileLayer
+//         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+//       />
+//       </Map>
+//     </Grid.Column>
+//     <Grid.Column>
+//       <Map refs='map' center={[0,100]} zoom={6} style={{height: 600, width: 600}}>
+//       <TileLayer
+//         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//         url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+//       />
+//       </Map>
+//     </Grid.Column>
+//   </Grid.Row>
+// </Grid>
+
+// <Grid padded>
+// <Grid.Row>
+//   <Header as='h1'>{this.state.programname}</Header>
+// </Grid.Row>
+// <Divider/>
+//
+// <Grid.Row>
+//   <Grid.Column width={6}>
+//
+//   <Map refs='map' center={[0,100]} zoom={6} style={{height: 600, width: 600}}>
+//   <TileLayer
+//     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//     url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+//   />
+//   </Map>
+//
+// </Grid.Column>
+// <Grid.Column width={10}>
+// More data here
+// </Grid.Column>
+//
+// </Grid.Row>
+// <Divider/>
+// <Grid.Row>
+//   <Container>
+//   <ProgramOverFeed/>
+//   </Container>
+// </Grid.Row>
+// </Grid>
--- orionWeb/src/ProgramOverFeed.js
+++ orionWeb/src/ProgramOverFeed.js
@@ -9,89 +9,45 @@ const ProgramOverFeed = () => (
       </Feed.Label>
       <Feed.Content>
         <Feed.Summary>
-          <Feed.User>Elliot Fu</Feed.User> added you as a friend
-          <Feed.Date>1 Hour Ago</Feed.Date>
+          <Feed.User>Arkar</Feed.User> completed a survey
+          <Feed.Date>2 days Ago</Feed.Date>
         </Feed.Summary>
-        <Feed.Meta>
-          <Feed.Like>
-            <Icon name='like' />
-            4 Likes
-          </Feed.Like>
-        </Feed.Meta>
       </Feed.Content>
     </Feed.Event>
 
     <Feed.Event>
-      <Feed.Label image='./helen.jpg' />
+      <Feed.Label>
+        <img src='./jenny.jpg' />
+      </Feed.Label>
       <Feed.Content>
         <Feed.Summary>
-          <a>Helen Troy</a> added <a>2 new illustrations</a>
-          <Feed.Date>4 days ago</Feed.Date>
+          <Feed.User>Chesa</Feed.User> completed a survey
+          <Feed.Date>3 days Ago</Feed.Date>
         </Feed.Summary>
-        <Feed.Extra images>
-          <a><img src='/assets/images/wireframe/image.png' /></a>
-          <a><img src='/assets/images/wireframe/image.png' /></a>
-        </Feed.Extra>
-        <Feed.Meta>
-          <Feed.Like>
-            <Icon name='like' />
-            1 Like
-          </Feed.Like>
-        </Feed.Meta>
       </Feed.Content>
     </Feed.Event>
 
     <Feed.Event>
-      <Feed.Label image='./jenny.jpg' />
-      <Feed.Content>
-        <Feed.Summary date='2 Days Ago' user='Jenny Hess' content='add you as a friend' />
-        <Feed.Meta>
-          <Feed.Like>
-            <Icon name='like' />
-            8 Likes
-          </Feed.Like>
-        </Feed.Meta>
-      </Feed.Content>
-    </Feed.Event>
-
-    <Feed.Event>
-      <Feed.Label image='./joe.jpg' />
+      <Feed.Label>
+        <img src='./joe.jpg' />
+      </Feed.Label>
       <Feed.Content>
         <Feed.Summary>
-          <a>Joe Henderson</a> posted on his page
-          <Feed.Date>3 days ago</Feed.Date>
+          <Feed.User>Bennu</Feed.User> completed a survey
+          <Feed.Date>1 week Ago</Feed.Date>
         </Feed.Summary>
-        <Feed.Extra text>
-          Ours is a life of constant reruns. We're always circling back to where we'd we started, then starting all
-          over again. Even if we don't run extra laps that day, we surely will come back for more of the same another
-          day soon.
-        </Feed.Extra>
-        <Feed.Meta>
-          <Feed.Like>
-            <Icon name='like' />
-            5 Likes
-          </Feed.Like>
-        </Feed.Meta>
       </Feed.Content>
     </Feed.Event>
 
     <Feed.Event>
-      <Feed.Label image='./justen.jpg' />
+      <Feed.Label>
+        <img src='./justen.jpg' />
+      </Feed.Label>
       <Feed.Content>
         <Feed.Summary>
-          <a>Justen Kitsune</a> added <a>2 new photos</a> of you
-          <Feed.Date>4 days ago</Feed.Date>
+          <Feed.User>Dachen</Feed.User> completed a survey
+          <Feed.Date>1 week Ago</Feed.Date>
         </Feed.Summary>
-        <Feed.Extra images>
-          <a><img src='/assets/images/wireframe/image.png' /></a>
-          <a><img src='/assets/images/wireframe/image.png' /></a>
-        </Feed.Extra>
-        <Feed.Meta>
-          <Feed.Like>
-            <Icon name='like' />
-            41 Likes
-          </Feed.Like>
-        </Feed.Meta>
       </Feed.Content>
     </Feed.Event>
   </Feed>
